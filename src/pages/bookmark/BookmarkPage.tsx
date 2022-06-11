ec0ebbd62fe4e0a841c2fe1f8122f3b2fdc7f051
Ivan Duran <idurandiaz@gmail.com>
2022-06-12 00:44:01 +0300

    Fetch basic bookmark page info

--- src/pages/bookmark/BookmarkPage.tsx
+++ src/pages/bookmark/BookmarkPage.tsx
@@ -1,5 +1,27 @@
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
+import { BookmarkType } from '../../utils/types';
+
 const BookmarkPage: React.FC = () => {
-  return <div>Bookmark...</div>;
+  const { id } = useParams<{ id: string }>();
+  const url = `http://localhost:3001/bookmarks/${id}`;
+  const {
+    data: bookmark,
+    isPending,
+    error,
+  } = useFetch<BookmarkType | null>(url, null);
+
+  return (
+    <div>
+      {error && <p className='text-center'>{error}</p>}
+      {isPending && <h3 className='text-center py-2'>Loading...</h3>}
+      {bookmark && (
+        <div>
+          <p className='py-5'>Bookmark Title: {bookmark.title}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default BookmarkPage;
